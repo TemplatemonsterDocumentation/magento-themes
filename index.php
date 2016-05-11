d418395a7723561e9ae0f0041dc5fc2a2ae5d5e7
makedonen <makedonen1@gmail.com>
2016-05-11 13:56:14 +0300

    upd

--- index.php
+++ index.php
@@ -48,8 +48,9 @@ if (!isset($sections)) {
         <div class="panel">
             <div class="select select-version">
                 <select>
-                    <option data-href="/help/quick-start-guide/magento-themes/v4-1/index_en.html">Version v4-1</option>
-                    <option data-href="/help/quick-start-guide/magento-themes/v4-0/index_en.html">Version v4-0</option>
+                    <option data-href="/help/quick-start-guide/magento-themes/v4-2/">Version v4-2</option>
+                    <option data-href="/help/quick-start-guide/magento-themes/v4-1/">Version v4-1</option>
+                    <option data-href="/help/quick-start-guide/magento-themes/v4-0/">Version v4-0</option>
                     <!-- <option data-href="/help/quick-start-guide/magento-themes/v1-2/index_en.html">Version v1-2</option>
                     <option data-href="/help/quick-start-guide/magento-themes/v2-0/index_en.html" selected>Version v2-0</option> -->
                 </select>
