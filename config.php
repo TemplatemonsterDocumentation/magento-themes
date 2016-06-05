d6309dc9af8a04d8b4dbe4275f53227fb3071617
makedonen <makedonen1@gmail.com>
2016-06-05 18:12:01 +0300

    ajax search

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $path = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']);
 
 // Variables
-$product_name = "Social Login documentation";
+$product_name = "Ajax Search documentation";
 $product_desc = "";
 $doc_title = $product_name . " " . $product_desc;
 
@@ -12,7 +12,7 @@ if (isset($_GET['lang'])) {
 	$lang = $_GET['lang'];
 }
 
-$section_param = 'social-login';
+$section_param = 'ajax-search';
 if (isset($_GET['section'])) {
 	$section_param = $_GET['section'];
 }
