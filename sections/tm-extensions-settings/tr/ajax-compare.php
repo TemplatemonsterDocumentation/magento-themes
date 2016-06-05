1cb950632b46181dfa927b752ce709d27bc5c96d
makedonen <makedonen1@gmail.com>
2016-06-05 17:59:05 +0300

    Only social login

--- sections/tm-extensions-settings/tr/ajax-compare.php
+++
@@ -1,6 +0,0 @@
-<h3>Ajax Compare</h3>
-<p>Ürün karþýlaþtýrmasý en standart e-ticaret özelliklerinden biridir. Bu özellik için pratik bir modül geliþtirdik. AJAX teknolojisine dayalý ve ayrý bir sayfaya gitmeden karþýlaþtýrma için ürün eklemenizi saðlýyor.</p>
- <p>Bu modülü açýp kapatabilirsiniz. Bunun için Magento yönetici panelindeki sol menüden <strong>Maðazalar > Ayarlar</strong> kýsmýna giriniz.</p>
- <figure class="img-polaroid"><img src="img/magento/compare.jpg" alt="" /></figure>
- <p>Daha sonra sol taraftaki <strong> TemplateMonster > Product Compare Ajax </strong> bölümünü seçiniz.</p>
- <figure class="img-polaroid"><img src="img/magento/compare-1.jpg" alt="" /></figure>
