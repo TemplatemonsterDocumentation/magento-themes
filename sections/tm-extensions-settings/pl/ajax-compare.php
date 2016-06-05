b5057c9bd313f7f00ceebb48b5f6dfe553abc7c4
makedonen <makedonen1@gmail.com>
2016-06-05 18:01:11 +0300

    Film slider

--- sections/tm-extensions-settings/pl/ajax-compare.php
+++
@@ -1,6 +0,0 @@
-<h3>Ajax Compare</h3>
-<p>Porównanie produktów jest jedną ze standardowych funkcji handlu elektroncznego (e-commerce). Stworzyliśmy specjalny moduł dla wygodnego korzystania z tej funkcji. Jest oparty na technologii AJAX, a także umożliwia dodawanie produktów do porównania bez konieczności przechodzenia na osobną stronę.</p>
- <p>Można włączyć lub wyłączyć ten moduł. W tym celu, należy przejść do panelu administracyjnego Magento i w menu po lewej stronie wybrać sekcję <strong>Sklepy > Konfiguracja (Stores > Configuration)</strong>.</p>
- <figure class="img-polaroid"><img src="img/magento/compare.jpg" alt="" /></figure>
- <p>Zatem na nowej stronie należy wybrać element <strong> TemplateMonster > Product Compare Ajax </strong> po lewej stronie.</p>
- <figure class="img-polaroid"><img src="img/magento/compare-1.jpg" alt="" /></figure>
