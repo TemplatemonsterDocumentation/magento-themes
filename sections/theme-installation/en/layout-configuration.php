dd2706aea4ba4f882afd883eec1e43e5a5c0da8a
makedonen <makedonen1@gmail.com>
2017-03-13 14:36:10 +0200

    Layouts

--- sections/theme-installation/en/layout-configuration.php
+++ sections/theme-installation/en/layout-configuration.php
@@ -208,7 +208,7 @@
             <td>Slider_layout_1</td>
             <td>Template Monster FilmSlider</td>
             <td>After Page Header</td>
-			<td>N/A</td>
+			<td>Slider_layout_1</td>
 			<td>1</td>
         </tr>
         <tr>
@@ -332,7 +332,7 @@
             <td>block_4_2_layout_1</td>
             <td>CMS Static Block</td>
             <td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_4_2_layout_1</td>
 			<td>10</td>
         </tr>
         <tr>
@@ -356,12 +356,13 @@
             <td>block_6_2_layout_2</td>
             <td>CMS Static Block</td>
             <td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_6_2_layout_2</td>
 			<td>40</td>
         </tr>
     </tbody>
 </table>
 <h5><em>Layout 3</em></h5>
+<a class="pretty_image" href="img/magento/furn-lay-3.jpg"><figure class="img-polaroid"><img src="img/magento/furn-lay-3.jpg" alt=""></figure></a>
 <h5><em>Static blocks Furniture layout 3</em></h5>
 <table class="data-table data-table-indent">
   <thead>
@@ -399,7 +400,7 @@
             <td>Slider_layout_1</td>
             <td>Template Monster FilmSlider</td>
             <td>After Page Header</td>
-			<td>N/A</td>
+			<td>Slider_layout_1</td>
 			<td>10</td>
         </tr>
         <tr>
@@ -407,7 +408,7 @@
             <td>block_5_2_layout_1</td>
             <td>CMS Static Block</td>
 			<td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_5_2_layout_1</td>
 			<td>10</td>
         </tr>
 		<tr>
@@ -488,7 +489,7 @@
             <td>Slider_layout_2</td>
             <td>Template Monster FilmSlider</td>
             <td>After Page Header</td>
-			<td>N/A</td>
+			<td>Slider_layout_2</td>
 			<td>1</td>
         </tr>
         <tr>
@@ -496,7 +497,7 @@
             <td>block_1_layout_2</td>
             <td>CMS Static Block</td>
 			<td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_1_layout_2</td>
 			<td>1</td>
         </tr>
 		<tr>
@@ -504,7 +505,7 @@
             <td>block_2_layout_2</td>
             <td>CMS Static Block</td>
 			<td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_2_layout_2</td>
 			<td>2</td>
         </tr>
         <tr>
@@ -528,7 +529,7 @@
             <td>block_3_layout_2</td>
             <td>CMS Static Block</td>
 			<td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_3_layout_2</td>
 			<td>7</td>
         </tr>
 		<tr>
@@ -596,7 +597,7 @@
             <td>Slider_layout_2</td>
             <td>Template Monster FilmSlider</td>
             <td>After Page Header</td>
-			<td>N/A</td>
+			<td>Slider_layout_2</td>
 			<td>1</td>
         </tr>
         <tr>
@@ -604,7 +605,7 @@
             <td>block_4_2_layout_2</td>
             <td>CMS Static Block</td>
 			<td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_4_2_layout_2</td>
 			<td>10</td>
         </tr>
 		<tr>
@@ -620,7 +621,7 @@
             <td>block_5_2_layout_2</td>
             <td>CMS Static Block</td>
             <td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_5_2_layout_2</td>
 			<td>30</td>
         </tr>
         <tr>
@@ -636,12 +637,16 @@
             <td>block_6_2_layout_2</td>
             <td>CMS Static Block</td>
 			<td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_6_2_layout_2</td>
 			<td>50</td>
         </tr>
     </tbody>
 </table>
-<h5><em>Widgets configuration  Lingerie layout 2</em></h5>
+
+<h5>Layout 3</h5>
+<p><a class="pretty_image" href="img/magento/ling-lay-3.jpg"><figure class="img-polaroid"><img src="img/magento/ling-lay-3.jpg" alt=""></figure></a>
+<h5><em>Widgets configuration  Lingerie layout 3</em></h5>
+
 <table class="data-table data-table-indent">
   <thead>
         <tr>
@@ -711,7 +716,7 @@
             <td>Slider_layout_3</td>
             <td>Template Monster FilmSlider</td>
             <td>After Page Header</td>
-			<td>N/A</td>
+			<td>Slider_layout_3</td>
 			<td>10</td>
         </tr>
         <tr>
@@ -719,7 +724,7 @@
             <td>block_1_layout_3</td>
             <td>CMS Static Block</td>
 			<td>After Page Header</td>
-			<td>N/A</td>
+			<td>block_1_layout_3</td>
 			<td>20</td>
         </tr>
 		<tr>
@@ -743,7 +748,7 @@
             <td>block_2_layout_3</td>
             <td>CMS Static Block</td>
             <td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_2_layout_3</td>
 			<td>30</td>
         </tr>
 		<tr>
@@ -757,7 +762,7 @@
     </tbody>
 </table>
 <h5><em>Layout 2</em></h5>
-<a class="pretty_image" href="img/magento/furn-lay-2.jpg"><figure class="img-polaroid"><img src="img/magento/bike-lay-2.jpg" alt=""></figure></a>
+<a class="pretty_image" href="img/magento/bike-lay-2.jpg"><figure class="img-polaroid"><img src="img/magento/bike-lay-2.jpg" alt=""></figure></a>
 <h5><em>Static blocks Bike layout 2</em></h5>
 <table class="data-table data-table-indent">
     <thead>
@@ -801,7 +806,7 @@
             <td>Slider_layout_3</td>
             <td>Template Monster FilmSlider</td>
             <td>After Page Header</td>
-			<td>N/A</td>
+			<td>Slider_layout_3</td>
 			<td>10</td>
         </tr>
         <tr>
@@ -825,7 +830,7 @@
             <td>block_3_2_layout_3</td>
             <td>CMS Static Block</td>
             <td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_3_2_layout_3</td>
 			<td>20</td>
         </tr>
         <tr>
@@ -841,7 +846,7 @@
             <td>block_4_2_layout_3</td>
             <td>CMS Static Block</td>
 			<td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_4_2_layout_3</td>
 			<td>40</td>
         </tr>
 		<tr>
@@ -855,6 +860,7 @@
     </tbody>
 </table>
 <h5><em>Layout 3</em></h5>
+<a class="pretty_image" href="img/magento/bike-lay-3.jpg.jpg"><figure class="img-polaroid"><img src="img/magento/bike-lay-3.jpg" alt=""></figure></a>
 <h5><em>Static blocks Bike layout 3</em></h5>
 <table class="data-table data-table-indent">
     <thead>
@@ -898,7 +904,7 @@
             <td>Slider_layout_3</td>
             <td>Template Monster FilmSlider</td>
             <td>After Page Header</td>
-			<td>N/A</td>
+			<td>Slider_layout_3</td>
 			<td>10</td>
         </tr>
         <tr>
@@ -906,7 +912,7 @@
             <td>block_5_3_layout_3</td>
             <td>CMS Static Block</td>
 			<td>After Page Header</td>
-			<td>N/A</td>
+			<td>block_5_3_layout_3</td>
 			<td>20</td>
         </tr>
 		<tr>
@@ -922,7 +928,7 @@
             <td>block_6_3_layout_3</td>
             <td>CMS Static Block</td>
             <td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_6_3_layout_3</td>
 			<td>20</td>
         </tr>
         <tr>
@@ -958,7 +964,7 @@
     </tbody>
 </table>
 <h5><em>Layout 1</em></h5>
-<a class="pretty_image" href="img/magento/furn-lay-2.jpg"><figure class="img-polaroid"><img src="img/magento/med-lay-2.jpg" alt=""></figure></a>
+<a class="pretty_image" href="img/magento/med-lay-1.jpg"><figure class="img-polaroid"><img src="img/magento/med-lay-1.jpg" alt=""></figure></a>
 <h5><em> Static blocks Medical layout 1
 </em></h5>
 <table class="data-table data-table-indent">
@@ -1009,7 +1015,7 @@
             <td>Slider_layout_4</td>
             <td>Template Monster FilmSlider</td>
             <td>Main Content Area</td>
-			<td>N/A</td>
+			<td>Slider_layout_4</td>
 			<td>10</td>
         </tr>
         <tr>
@@ -1017,7 +1023,7 @@
             <td>block_1_layout_4</td>
             <td>CMS Static Block</td>
 			<td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_1_layout_4</td>
 			<td>20</td>
         </tr>
 		<tr>
@@ -1033,7 +1039,7 @@
             <td>block_2_layout_4</td>
             <td>CMS Static Block</td>
             <td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_2_layout_4</td>
 			<td>40</td>
         </tr>
         <tr>
@@ -1071,7 +1077,7 @@
     </tbody>
 </table>
 <h5><em>Layout 2</em></h5>
-<a class="pretty_image" href="img/magento/furn-lay-2.jpg"><figure class="img-polaroid"><img src="img/magento/med-lay-2.jpg" alt=""></figure></a>
+<a class="pretty_image" href="img/magento/med-lay-2.jpg"><figure class="img-polaroid"><img src="img/magento/med-lay-2.jpg" alt=""></figure></a>
 <h5><em>Static blocks Medical layout 2</em></h5>
 <table class="data-table data-table-indent">
     <thead>
@@ -1115,7 +1121,7 @@
             <td>Slider_layout_4</td>
             <td>Template Monster FilmSlider</td>
             <td>Main Content Area</td>
-			<td>N/A</td>
+			<td>Slider_layout_4</td>
 			<td>10</td>
         </tr>
         <tr>
@@ -1123,7 +1129,7 @@
             <td>block_1_layout_4</td>
             <td>CMS Static Block</td>
 			<td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_1_layout_4</td>
 			<td>20</td>
         </tr>
 		<tr>
@@ -1153,6 +1159,7 @@
     </tbody>
 </table>
 <h5><em>Layout 3</em></h5>
+<a class="pretty_image" href="img/magento/med-lay-3.jpg"><figure class="img-polaroid"><img src="img/magento/med-lay-3.jpg" alt=""></figure></a>
 <h5><em>Static blocks Medical layout 3</em></h5>
 <table class="data-table data-table-indent">
     <thead>
@@ -1196,7 +1203,7 @@
             <td>Slider_layout_4</td>
             <td>Template Monster FilmSlider</td>
             <td>Main Content Area</td>
-			<td>N/A</td>
+			<td>Slider_layout_4</td>
 			<td>10</td>
         </tr>
         <tr>
@@ -1212,7 +1219,7 @@
             <td>block_2_layout_4</td>
             <td>CMS Static Block</td>
 			<td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_2_layout_4</td>
 			<td>30</td>
         </tr>
         <tr>
@@ -1228,12 +1235,14 @@
             <td>block_5_3_layout_4</td>
             <td>CMS Static Block</td>
 			<td>After Main Columns</td>
-			<td>N/A</td>
+			<td>block_2_layout_4</td>
 			<td>50</td>
         </tr>
     </tbody>
 </table>
 <h4>Spare Parts </h4>
+<h5><em>Layout 1</em></h5>
+<a class="pretty_image" href="img/magento/spp-lay-1.jpg"><figure class="img-polaroid"><img src="img/magento/spp-lay-1.jpg" alt=""></figure></a>
 <h5><em>Static blocks Spare Parts layout 1</em></h5>
 <table class="data-table data-table-indent">
     <thead>
@@ -1271,7 +1280,7 @@
             <td>Slider_Spare_Pats</td>
             <td>Template Monster FilmSlider</td>
             <td>After Page Header</td>
-			<td>N/A</td>
+			<td>Slider_Spare_Pats</td>
 			<td>10</td>
         </tr>
         <tr>
@@ -1303,7 +1312,7 @@
             <td>block_1_1_layout_5</td>
             <td>CMS Static Block</td>
             <td>After Page Header</td>
-			<td>N/A</td>
+			<td>block_1_1_layout_5</td>
 			<td>30</td>
         </tr>
 		<tr>
@@ -1316,6 +1325,8 @@
         </tr>
     </tbody>
 </table>
+<h5><em>Layout 2</em></h5>
+<a class="pretty_image" href="img/magento/spp-lay-2.jpg"><figure class="img-polaroid"><img src="img/magento/spp-lay-2.jpg" alt=""></figure></a>
 <h5><em>Static blocks Spare Parts layout 2</em></h5>
 <table class="data-table data-table-indent">
     <thead>
@@ -1362,7 +1373,7 @@
             <td>Slider Spare parts lt_2</td>
             <td>Template Monster FilmSlider</td>
             <td>Main Content Area</td>
-			<td>N/A</td>
+			<td>Slider Spare parts lt_2</td>
 			<td>10</td>
         </tr>
         <tr>
@@ -1370,7 +1381,7 @@
             <td>block_1_2_layout_5</td>
             <td>CMS Static Block</td>
 			<td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_1_2_layout_5</td>
 			<td>20</td>
         </tr>
 		<tr>
@@ -1402,11 +1413,13 @@
             <td>block_2_2_layout_5</td>
             <td>CMS Static Block</td>
 			<td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_2_2_layout_5</td>
 			<td>60</td>
         </tr>
     </tbody>
 </table>
+<h5><em>Layout 3</em></h5>
+<a class="pretty_image" href="img/magento/spp-lay-3.jpg"><figure class="img-polaroid"><img src="img/magento/spp-lay-3.jpg" alt=""></figure></a>
 <h5><em>Widgets configuration Spare Parts layout 3</em></h5>
 <table class="data-table data-table-indent">
   <thead>
@@ -1446,10 +1459,10 @@
         </tr>
         <tr>
             <td>4.</td>
-            <td>Parallax Block</td>
             <td>1_parallax_layout_5</td>
+            <td>Parallax Block</td>
             <td>Main Content Area</td>
-			<td>N/A</td>
+			<td>1_parallax_layout_5</td>
 			<td>30</td>
         </tr>
         <tr>
@@ -1462,10 +1475,10 @@
         </tr>
 		<tr>
             <td>6.</td>
-            <td>Parallax Block</td>
             <td>2_parallax_layout_5</td>
+            <td>Parallax Block</td>
 			<td>Main Content Area</td>
-			<td>N/A</td>
+			<td>2_parallax_layout_5</td>
 			<td>50</td>
         </tr>
         <tr>
@@ -1473,7 +1486,7 @@
             <td>block_2_2_layout_5</td>
             <td>CMS Static Block</td>
 			<td>Main Content Area</td>
-			<td>N/A</td>
+			<td>block_2_2_layout_5</td>
 			<td>60</td>
         </tr>
     </tbody>
